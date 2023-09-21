44420593ec017203b8c69051bc1c6e4cbc33da4e
张雅茹 <zhangyaru@zhangyarudeMacBook-Pro.local>
2023-09-21 14:24:02 +0800

    login

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -1,13 +1,20 @@
 <template>
 	<view class="content">
 		<view class="top-bar">
-			<view class="top-bar-left">
+			<view class="top-bar-left" @click="backTo">
 				<image src="http://47.98.128.255:3001/svg/close.svg"></image>
 			</view>
-			<view class="top-bar-right">
+			<view class="top-bar-right" @click="register">
 				<text>注册</text>
 				<!-- <image src="http://47.98.128.255:3001/png/logo.png"></image> -->
 			</view>
+		</view>
+		<view class="main">
+			<view class="login-text">登录</view>
+			<view class="login-slogan">欢迎登录慢煮！</view>
+			<input class="login-input" maxlength="10" placeholder="用户名/邮箱" />
+			<input class="login-input" password type="text" placeholder="密码" />
+			<button class="login-button" type="primary" @click="login" loading="true">登录</button>
 		</view>
 	</view>
 </template>
@@ -18,6 +25,15 @@
 			return {
 				
 			};
+		},
+		methods: {
+			backTo: function() {
+				uni.navigateBack({
+					delta: 1
+				});
+			},
+			register: function(){},
+			login: function(){},
 		}
 	}
 </script>
@@ -53,7 +69,6 @@
 			margin: 15rpx 20rpx;
 		}
 	}
-
 	.top-bar-right {
 		text {
 			width: 56rpx;
@@ -62,6 +77,29 @@
 			margin: 15rpx 20rpx;
 		}
 	}
-
-	
-}</style>
+}
+.main {
+	width: 100%;
+	box-sizing: border-box;
+	padding: 150rpx 32rpx;
+	.login-text {
+		font-size: 40rpx;
+		font-weight: 600;
+		width: 100%;
+		text-align: center;
+	}
+	.login-slogan {
+		color: $uni-text-color-grey;
+		margin: 20rpx 20rpx 80rpx 20rpx;
+	}
+	.login-input {
+		border-bottom: 1px solid $uni-border-color;
+		line-height: 80rpx;
+		height: 80rpx;
+		padding-left: 20rpx;
+	}
+	.login-button{
+		margin: 50rpx 0;
+	}
+}
+</style>
